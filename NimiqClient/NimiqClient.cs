3120859d18c61ea62a3324b2fe5976997553ab4d
Developer <>
2020-07-17 07:46:33 -0400

    add methods

--- NimiqClient/NimiqClient.cs
+++ NimiqClient/NimiqClient.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Text;
 using System.Text.Json;
 using System;
+using System.Collections.Generic;
 
 namespace Nimiq
 {
@@ -24,6 +25,154 @@ namespace Nimiq
         public T result { get; set; }
         public int id { get; set; }
         public ResponseError error { get; set; }
+    }
+
+    /// <summaryType of a Nimiq account.</summary>
+    [Serializable]
+    public enum AccountType : int
+    {
+        /// <summaryNormal Nimiq account.</summary>
+        basic = 0,
+        /// <summaryVesting contract.</summary>
+        vesting = 1,
+        /// <summaryHashed Timelock Contract.</summary>
+        htlc = 2,
+    }
+
+    /// <summary>Normal Nimiq account object returned by the server.</summary>
+    public class Account
+    {
+        /// <summary>Hex-encoded 20 byte address.</summary>
+        public string id { get; set; }
+        /// <summary>User friendly address (NQ-address).</summary>
+        public string address { get; set; }
+        /// <summary>Balance of the account (in smallest unit).</summary>
+        public int balance { get; set; }
+        /// <summary>The account type associated with the account.</summary>
+        public AccountType type { get; set; }
+    }
+
+    /// <summary>Vesting contract object returned by the server.</summary>
+    public class VestingContract : Account
+    {
+        /// <summary>Hex-encoded 20 byte address of the owner of the vesting contract.</summary>
+        public string owner { get; set; }
+        /// <summary>User friendly address (NQ-address) of the owner of the vesting contract.</summary>
+        public string ownerAddress { get; set; }
+        /// <summary>The block that the vesting contracted commenced.</summary>
+        public int vestingStart { get; set; }
+        /// <summary>The number of blocks after which some part of the vested funds is released.</summary>
+        public int vestingStepBlocks { get; set; }
+        /// <summary>The amount (in smallest unit) released every vestingStepBlocks blocks.</summary>
+        public int vestingStepAmount { get; set; }
+        /// <summary>The total amount (in smallest unit) that was provided at the contract creation.</summary>
+        public int vestingTotalAmount { get; set; }
+    }
+
+    /// <summary>Hashed Timelock Contract object returned by the server.
+    public class HTLC : Account
+    {
+        /// <summary>Hex-encoded 20 byte address of the sender of the HTLC.</summary>
+        public string sender { get; set; }
+        /// <summary>User friendly address (NQ-address) of the sender of the HTLC.</summary>
+        public string senderAddress { get; set; }
+        /// <summary>Hex-encoded 20 byte address of the recipient of the HTLC.</summary>
+        public string recipient { get; set; }
+        /// <summary>User friendly address (NQ-address) of the recipient of the HTLC.</summary>
+        public string recipientAddress { get; set; }
+        /// <summary>Hex-encoded 32 byte hash root.</summary>
+        public string hashRoot { get; set; }
+        /// <summary>Hash algorithm.</summary>
+        public int hashAlgorithm { get; set; }
+        /// <summary>Number of hashes this HTLC is split into.</summary>
+        public int hashCount { get; set; }
+        /// <summary>Block after which the contract can only be used by the original sender to recover funds.</summary>
+        public int timeout { get; set; }
+        /// <summary>The total amount (in smallest unit) that was provided at the contract creation.</summary>
+        public int totalAmount { get; set; }
+    }
+
+    /// <summary>Nimiq account returned by the server. The especific type can obtained with the cast operator.<summary>
+    [Serializable]
+    class RawAccount
+    {
+
+        public string id { get; set; }
+        public string address { get; set; }
+        public int balance { get; set; }
+        public AccountType type { get; set; }
+        public string owner { get; set; }
+        public string ownerAddress { get; set; }
+        public int vestingStart { get; set; }
+        public int vestingStepBlocks { get; set; }
+        public int vestingStepAmount { get; set; }
+        public int vestingTotalAmount { get; set; }
+        public string sender { get; set; }
+        public string senderAddress { get; set; }
+        public string recipient { get; set; }
+        public string recipientAddress { get; set; }
+        public string hashRoot { get; set; }
+        public int hashAlgorithm { get; set; }
+        public int hashCount { get; set; }
+        public int timeout { get; set; }
+        public int totalAmount { get; set; }
+
+        /// <summary>Converts to normal account.<summary>
+        /// <param name="account">Raw account type with all fields.</param>
+        /// <returns>Normal account type.</returns>
+        public static explicit operator Account(RawAccount account)
+        {
+            return new Account()
+            {
+                id = account.id,
+                address = account.address,
+                balance = account.balance,
+                type = account.type,
+            };
+        }
+
+        /// <summary>Converts to vesting contract account.<summary>
+        /// <param name="account">Raw account type with all fields.</param>
+        /// <returns>Vesting contract account type.</returns>
+        public static explicit operator VestingContract(RawAccount account)
+        {
+            return new VestingContract()
+            {
+                id = account.id,
+                address = account.address,
+                balance = account.balance,
+                type = account.type,
+                owner = account.owner,
+                ownerAddress = account.ownerAddress,
+                vestingStart = account.vestingStart,
+                vestingStepBlocks = account.vestingStepBlocks,
+                vestingStepAmount = account.vestingStepAmount,
+                vestingTotalAmount = account.vestingTotalAmount
+            };
+        }
+
+        /// <summary>Converts to HTLC account.<summary>
+        /// <param name="account">Raw account type with all fields.</param>
+        /// <returns>HTLC account type.</returns>
+        public static explicit operator HTLC(RawAccount account)
+        {
+            return new HTLC()
+            {
+                id = account.id,
+                address = account.address,
+                balance = account.balance,
+                type = account.type,
+                sender = account.sender,
+                senderAddress = account.senderAddress,
+                recipient = account.recipient,
+                recipientAddress = account.recipientAddress,
+                hashRoot = account.hashRoot,
+                hashAlgorithm = account.hashAlgorithm,
+                hashCount = account.hashCount,
+                timeout = account.timeout,
+                totalAmount = account.totalAmount
+            };
+        }
     }
 
     // JSONRPC Client
@@ -155,6 +304,29 @@ namespace Nimiq
         /// <returns>Consensus state. <c>"established"</c> is the value for a good state, other values indicate bad.</returns>
         public async Task<string> Consensus() {
             return await Fetch<string>("consensus", new object[0]);
+        }
+
+        /// <summary>Returns a list of addresses owned by client.</summary>
+        /// <returns>Array of Accounts owned by the client.</returns>
+        public async Task<object[]> Accounts() {
+            var result = await Fetch<object[]>("accounts", new object[0]);
+            var converted = new object[0];
+            for (int i = 0; i < result.Length; i++) {
+                RawAccount account = (RawAccount)result[i];
+                switch (account.type)
+                {
+                    case AccountType.basic:
+                        converted[i] = (Account)account;
+                        break;
+                    case AccountType.vesting:
+                        converted[i] = (VestingContract)account;
+                        break;
+                    case AccountType.htlc:
+                        converted[i] = (HTLC)account;
+                        break;
+                }
+            }
+            return converted;
         }
     }
 }
